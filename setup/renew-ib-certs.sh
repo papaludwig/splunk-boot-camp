94b46c387b454d1b0eb6a003c677cce0c15b2bb8
Christian Ludwig <cludwig@instantbrains.com>
2026-02-08 16:52:19 -0500

    Create renew-ib-certs.sh for SSL certificate renewal
    Add script to renew SSL certificates for instantbrains.com

---
+++ setup/renew-ib-certs.sh
@@ -0,0 +1,4 @@
+sudo env AWS_PROFILE=default .certbot-env/bin/certbot renew --cert-name instantbrains.com
+sudo cp /etc/letsencrypt/live/instantbrains.com/fullchain.pem  ~/ib-fullchain.pem
+sudo cp /etc/letsencrypt/live/instantbrains.com/privkey.pem     ~/ib-privkey.pem
+chmod 600 ~/ib-*.pem
