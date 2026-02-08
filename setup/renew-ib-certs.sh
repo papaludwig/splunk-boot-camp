1451dcccaeeda3fedd9f3924dd1e19900c89983c
Christian Ludwig <cludwig@instantbrains.com>
2026-02-08 16:54:50 -0500

    Change ownership and permissions for cert files

--- setup/renew-ib-certs.sh
+++ setup/renew-ib-certs.sh
@@ -1,4 +1,5 @@
 sudo env AWS_PROFILE=default .certbot-env/bin/certbot renew --cert-name instantbrains.com
 sudo cp /etc/letsencrypt/live/instantbrains.com/fullchain.pem  ~/ib-fullchain.pem
 sudo cp /etc/letsencrypt/live/instantbrains.com/privkey.pem     ~/ib-privkey.pem
-sudo chmod 600 ~/ib-*.pem
+sudo chown "$USER":staff ~/ib-*.pem
+chmod 600 ~/ib-*.pem
